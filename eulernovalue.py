a301cf3df9e3c56e57bd17836e8413fc41719b72
AJAYBANIYAL <at725594@gmail.com>
2021-05-20 06:16:22 -0700

    Create eulernovalue.py
    output:2.71828

---
+++ eulernovalue.py
@@ -0,0 +1,20 @@
+
+#this code is for to find the euler no. in python
+#we can also use math.pow(1+1/n,n)
+#~Ajay Baniyal
+import math
+def euler(n):
+     sum=0
+     for i in range(n):
+         fact=1/math.factorial(i)
+         sum=sum+tearm
+
+     return sum
+n=int(input("(Enter no. of limit ):: "))
+e=euler(n)
+print("e=",e)
+
+
+
+        
+
